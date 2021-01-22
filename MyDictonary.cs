dc07c0993d52f6326fb71e521297e606f75d289a
vlikcc <vlikcc@VLIKCCPC>
2021-01-22 13:27:12 +0300

    Add project files.

---
+++ MyDictonary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictonary
+{
+    class MyDictonary <T>
+    {
+        T[] things;
+        public MyDictonary()
+        {
+            things = new T[0];
+        }
+        public void Add(T thing)
+        {
+            T[] temp = things;
+            things = new T[things.Length + 1];
+            for (int i = 0; i < temp.Length; i++)
+            {
+                things[i] = temp[i];
+            }
+            things[things.Length - 1] = thing;
+        }
+    }
+}
